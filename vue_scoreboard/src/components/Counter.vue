309ae776bd3967d8bc1f7ac0d6e899c179e15fc8
Rocha <jrocha@proforma.com>
2018-09-04 23:03:31 -0400

    ExplorerMode

---
+++ vue_scoreboard/src/components/Counter.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h1>{{count}}</h1>
+        <button v-on:click="addOne">Add 1!</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Counter",
+    data:function(){
+        return {
+            count:0,
+        }
+    },
+    methods:{
+        addOne:function(){
+            this.count++;
+        },
+        subtractOne:function(){
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+h1{
+    color:yellow;
+    margin-top: 5px;
+    }
+button {
+    background-color: lightblue;
+    }
+</style>
