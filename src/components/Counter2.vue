df93446750416238677781d533bc4fa345b08cab
Rocha <jrocha@proforma.com>
2018-09-04 23:11:34 -0400

    Accidentally moved '.git' folder into assignments folder.  Nightmare.

---
+++ src/components/Counter2.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1>{{count}}</h1>
+        <button v-on:click="addOne">Add 1!</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Counter2",
+    data:function(){
+        return {
+            count:0,
+        }
+    },
+    methods:{
+        addOne:function(){
+            this.count++;
+        },
+        subtractOne:function(){
+        }
+    }
+}
+</script>
+
+<style scoped>
+    h1{
+      color: yellow;
+      margin-top: 5px;
+      }
+  button {
+      background-color: lightblue;
+      }
+</style>
---
+++ src/components/PlayerName.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <input type="text" placeholder="Name Here" v-model="playerName" />
+    <button v-on:click="inputName">Change Team Name </button>
+    <h2>{{nameDisplay}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'playerName',
+  data:function(){
+    return {
+    playerName:"",
+    nameDisplay: "Team 1"
+  }
+},
+methods:{
+  inputName : function(){
+    this.nameDisplay = this.playerName
+  }
+ }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.flex {
+  display: flex;
+  justify-content: space-around;
+}
+
+h1 {
+  border: 1px solid black;
+  font-size: 24px;
+  color: yellow;
+}
+
+input{
+  padding: 3px;
+}
+
+button {
+  margin: 15px;
+  padding: 3px;
+  background-color: yellow;
+}
+
+</style>
